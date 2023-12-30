057c990ead1904201b663d0b67913b2ae07a8bab
Mehran Piltan <110996599+MagicalAtom@users.noreply.github.com>
2023-12-30 11:56:37 +0330

    Update tabar.dart

--- lib/components/tabar.dart
+++ lib/components/tabar.dart
@@ -1,3 +1,4 @@
+/// bottom tabBar advertise library . Button Tab in advertise Screen
 library easy_animated_tabbar;
 import 'package:easy_animated_tabbar/easy_button_widget.dart';
 import 'package:flutter/material.dart';
